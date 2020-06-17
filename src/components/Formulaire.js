d015b6104064dbac6ffa45e58d8cb1acda117c7e
amineneghbel <amineneghbel@gmail.com>
2020-06-17 20:17:44 +0200

    firebase en cours d'initialisation

---
+++ src/components/Formulaire.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+class Formulaire extends Component {
+    
+    state = {
+        message: '',
+        length: this.props.length
+    }
+    
+    createMessage = () => {
+        const { addMessage, pseudo, length } = this.props
+
+        const message = {
+            pseudo,
+            message: this.state.message
+        }
+        addMessage(message)
+        // reset
+        this.setState({ message: '', length })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.createMessage()
+
+    }
+
+    handleChange = event => {
+        const message = event.target.value
+        const length = this.props.length - message.length
+        this.setState({ message, length })
+    }
+
+    handleKeyUp = event => {
+        if (event.key === 'Enter') {
+            this.createMessage()
+        }
+    }
+
+    render() {
+        return (
+            <form className="form"
+                onSubmit={this.handleSubmit} >
+            <textarea 
+                value={this.state.message}
+                required
+                maxLength={this.props.length}
+                onChange={this.handleChange}
+                onKeyUp={this.handleKeyUp} />
+            <div className="info">
+                {this.state.length}
+            </div>
+            <button type="submit">
+                 Envoyer!
+            </button>
+            </form>
+        );
+    }
+}
+
+export default Formulaire;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import Connexion from './components/Connexion'
 import * as serviceWorker from './serviceWorker'
+import NotFound from './components/NotFound'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+const Root = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={Connexion} />
+            <Route exact path='/pseudo/:pseudo' component={App} />
+            <Route component={NotFound} />
+        </Switch>
+    </BrowserRouter>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
